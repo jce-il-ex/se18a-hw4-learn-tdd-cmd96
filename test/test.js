480cdbbafde6f26991b0fa5065ae8bb18ff26491
chemda <chemda96@gmail.com>
2018-01-08 18:29:35 +0200

    RED - Cash Register

--- test/test.js
+++ test/test.js
@@ -1,6 +1,20 @@
 var assert = require("assert"); // node.js core module
 var coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 var C = require('../cash.js');  // our module
+
+var assert = require("assert"); // core module
+var C = require('../cash.js');  // our module
+
+describe('Cash Register', function(){
+  describe('Module C', function(){
+    it('should have a getChange Method', function(){
+      assert.equal(typeof C, 'object');
+      assert.equal(typeof C.getChange, 'function');
+    })
+  })
+});  
+
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
