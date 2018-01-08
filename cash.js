58b3df55f78fb455aa3e6e00c1b0994c87302afc
chemda <chemda96@gmail.com>
2018-01-08 18:37:00 +0200

    RED & GREEN - should have a getChange Method

--- cash.js
+++ cash.js
@@ -1,4 +1,14 @@
-var C = {};                    // C Object simplifies exporting the module
+var C = {};   
+
+
+
+totalPayable = 210         // £2.10
+cashPaid     = 300         // £3.00
+difference   =  90         // 90p
+change       = [50,20,20]  // 50p, 20p, 20p
+
+
+// C Object simplifies exporting the module
 C.getChange = function () {    // enough to satisfy the test
     'use strict';
     return true;               // also passes JSLint
--- test/test.js
+++ test/test.js
@@ -5,21 +5,19 @@ var C = require('../cash.js');  // our module
 var assert = require("assert"); // core module
 var C = require('../cash.js');  // our module
 
+
+
 describe('Cash Register', function(){
   describe('Module C', function(){
     it('should have a getChange Method', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
     })
+
+    it('getChange(210,300) should equal [50,20,20]', function(){
+        assert.deepEqual(C.getChange(210,300), [50,20,20]);
+    })
   })
 });  
 
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
-    })
-  })
-  
-});
